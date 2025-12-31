4c01cb7cc12df92ebeae731e00381c54c326ff08
okabe <azozf55@gmail.com>
2025-12-31 06:24:36 +0300

    LLM component is ready for backend intgration

--- idea1_walkabilityScoring/LLM_backend.py
+++ idea1_walkabilityScoring/LLM_backend.py
@@ -1,4 +1,5 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
+import time
 
 # Load model and tokenizer
 model_id = "LiquidAI/LFM2-1.2B"
@@ -11,14 +12,14 @@ model = AutoModelForCausalLM.from_pretrained(
 tokenizer = AutoTokenizer.from_pretrained(model_id)
 
 # Generate answer
-prompt = "What is C. elegans?"
+prompt = "كلمني عن تاريخ الرياض"
 input_ids = tokenizer.apply_chat_template(
     [{"role": "user", "content": prompt}],
     add_generation_prompt=True,
     return_tensors="pt",
     tokenize=True,
 ).to(model.device)
-
+time_start = time.time()
 output = model.generate(
     input_ids,
     do_sample=True,
@@ -27,11 +28,12 @@ output = model.generate(
     repetition_penalty=1.05,
     max_new_tokens=512,
 )
-
+time_end = time.time()
+time_taken = time_end - time_start
+print(f"Time taken: {time_taken:.2f} seconds")
 print(tokenizer.decode(output[0], skip_special_tokens=False))
 
-# <|startoftext|><|im_start|>user
-# What is C. elegans?<|im_end|>
-# <|im_start|>assistant
+# <|startoftext|>1user
+# What is C. elegans? 
 # C. elegans, also known as Caenorhabditis elegans, is a small, free-living
 # nematode worm (roundworm) that belongs to the phylum Nematoda.
